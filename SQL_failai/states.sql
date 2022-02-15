759e285f342067a413ba0262e1256cb11a91f844
Tautvydas Dulskis <t.dulskis@salvum.lt>
2022-02-15 19:42:36 +0200

    SQL failai

---
+++ SQL_failai/states.sql
@@ -0,0 +1,8 @@
+create table states (
+	id INT,
+	title VARCHAR(50),
+	type VARCHAR(50)
+);
+insert into states (id, title, type) values (1, 'Voltsillam', 'Bigtax');
+insert into states (id, title, type) values (2, 'Stronghold', 'Tampflex');
+insert into states (id, title, type) values (3, 'Biodex', 'Overhold');
